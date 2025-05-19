9bd9bf39d5f57720bc13176234dfe390291bee87
Arikanto2 <fahazavanaandriantsoa@gmail.com>
2025-05-19 16:16:17 +0300

    message

--- src/views/ajout_emp.vue
+++ src/views/ajout_emp.vue
@@ -1,3 +1,19 @@
 <template>
-  <p>hi ajout</p>
+  <div>
+    <form action="">
+      <div class="container">
+        <label for=""></label>
+        <input type="text" />
+      </div>
+      <div class="container">
+        <label for=""></label>
+        <input type="text" />
+      </div>
+      <div class="container">
+        <label for=""></label>
+        <input type="text" />
+      </div>
+      <input type="submit" />
+    </form>
+  </div>
 </template>
