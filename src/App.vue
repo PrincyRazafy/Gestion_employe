e6043245dcaf8ebefd02fc04afc595c64bc13700
PrincyRazafy <razafy805@gmail.com>
2025-05-19 14:52:28 +0300

    ajout axios

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="container mt-5">
+    <h1 class="text-primary">Bienvenue sur mon site Vue + Bootstrap !</h1>
+    <button>Clique ici</button>
+  </div>
+  <i class="fas fa-user"></i>
+  <i class="fab fa-github"></i>
+  <i class="fas fa-check-circle text-success"></i>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
