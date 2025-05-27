b225a445ee2fa0cd48bf7ec0a9b1b783956b11aa
Arikanto2 <fahazavanaandriantsoa@gmail.com>
2025-05-27 22:40:28 +0300

    Vita amzay le login sy deconnexion

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,9 @@
             >Ajout</router-link
           >
           <router-link class="nav-link text-white" to="/MAJ">MAJ</router-link>
-          <router-link class="nav-link text-white" to="/graphe">Graphe</router-link>
+          <router-link class="nav-link text-white" to="/graphe"
+            >Graphe</router-link
+          >
         </div>
       </div>
       <footer
@@ -30,19 +32,18 @@
     <div class="container center mt-3 pt-5">
       <router-view />
     </div>
+    <footer
+      class="bg-dark text-white py-2 white-shadow fixed-bottom text-center"
+    >
+      <div class="container d-flex justify-content-between align-items-center">
+        <button class="btn-logout" @click="logout">
+          <i class="fas fa-power-off"></i>
+        </button>
+        <p class="mb-0">&copy; 2025</p>
+      </div>
+    </footer>
   </div>
-  <footer class="bg-dark text-white py-2 white-shadow fixed-bottom text-center">
-    <div class="container d-flex justify-content-between align-items-center">
-      <button class="btn-logout" @click="logout">
-        <i class="fas fa-power-off"></i>
-      </button>
-      <p class="mb-0">&copy; 2025</p>
-    </div>
-  </footer>
 </template>
-<script setup>
-import typed from "./components/typed_js.vue";
-</script>
 <style scoped>
 .btn-logout {
   background-color: #ffe082;
@@ -77,6 +78,7 @@ import { ref, onMounted } from "vue";
 import login from "./views/login_page.vue";
 import { useRouter } from "vue-router";
 import Swal from "sweetalert2";
+import typed from "./components/typed_js.vue";
 
 const isLoggedIn = ref(false);
 const router = useRouter();
